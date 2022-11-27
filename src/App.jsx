2c04d97a996b1733066c22718d4a60b34c6e519e
AidenStripes <84007924+AidenStripes@users.noreply.github.com>
2022-11-27 22:05:35 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from "axios";
+
+function App() {
+  
+const client = axios.create({
+  baseURL: "http://localhost:8080/books" 
+});
+  
+  const [books, setBooks] = useState([]);
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(author, title);
+  };
+
+  const addBook = (author, title) => {
+    client
+      .post('', {
+          author: author,
+          title: title,
+        })
+        .then((response) => {
+          setBooks([response.data, ...books]);
+        });
+    setAuthor('');
+    setTitle('');
+  };
+
+  useEffect(() => {
+    client.get().then((response) => {
+        setBooks(response.data);
+      });
+  }, []);
+
+  const deleteBook = (id) => {
+    client.delete(`${id}`)
+    setBooks(
+      books.filter((book) => {
+        return book.id !== id;
+      })
+    )
+  }
+
+  return (
+    <div className="App">
+      <h2>All Books</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="author" placeholder="author" value={author}
+          onChange={(e) => setAuthor(e.target.value)}></input>
+        <input type="text" name="title" placeholder="title" value={title}
+          onChange={(e) => setTitle(e.target.value)}></input>
+        <button className="btn btn-primary" type="submit">add a book</button>
+      </form>
+      {books.map((book) => {
+      return (
+        <div key={book.id}>
+            <h2>{book.author}</h2>
+            <p>{book.title}</p>
+            <div className="button">
+              <div className="delete-btn" onClick={() => deleteBook(book.id)}>Delete</div>
+            </div>
+          </div>
+      );
+    })}
+    </div>
+  )
+}
+
+export default App
